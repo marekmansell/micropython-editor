4799d5b8282a86cef54665ed7c5ae6b47f578668
Marek Mansell <marek.mansell@gmail.com>
2017-06-24 00:04:49 +0200

    blockly in qt app support added

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 MicroPython Editor
-=====================
+==================
 
 
 The lastest builds for Windows, Linux and MacOS can be found here:
@@ -7,7 +7,7 @@ The lastest builds for Windows, Linux and MacOS can be found here:
 http://micropython.sk/instalacia/micropython_editor.html
 
 About MicroPython Editor
-------------------------------
+------------------------
 
 The editor is a simple tool for developing on the microPython platform.
 It is specifically designed for kids, teachers and beginners.
@@ -15,13 +15,13 @@ The editor was inspsired by the "Mu-Editor" (only for the bbc:microbit).
 
 
 Features
----------------------------
+--------
 * Download and code! You do not have to play with any serial settings (however, installing a driver for your board might be necessary)
 * MicroPython REPL
 
 
 What boards can I program?
-------------------------------------
+--------------------------
 
 Any microPython board which has REPL over USB support.
 
@@ -34,7 +34,7 @@ If you have tested the software with different hardware (successfully or not) pl
 
 
 Development
--------------------
+-----------
 
 MicroPython Editor is currently under heavy development, many features and bug-fixes are yet to be added.
 The project is open-source, so feel free to use it, test it or develop it!
--- editor/editor.py
+++ editor/editor.py
@@ -1,2 +1,61 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplashScreen, QMessageBox, QDesktopWidget, qApp, QAction
+from PyQt5.QtGui import QPixmap, QIcon
+# from PyQt5.
+
+
+def center_widget(widget):
+    qr = widget.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    widget.move(qr.topLeft())
+
+class EditorWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setup_ui()
+
+    def setup_ui(self):
+
+
+
+
+        exitAction = QAction(QIcon('editor/a.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+
+
+
+        self.setMinimumSize(800, 400)
+        center_widget(self)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('editor/a.png'))
+        self.statusBar()
+
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?",
+                                     QMessageBox.Yes or QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+
 def run():
-	print("running!")
+    print("running!")
+    app = QApplication(sys.argv)
+    splash = QSplashScreen(QPixmap('editor/a.png'))
+    splash.show()
+    editor_window = EditorWindow()
+    splash.finish(editor_window)
+    sys.exit(app.exec_())
